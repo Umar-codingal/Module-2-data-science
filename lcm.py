0f21459f2a1269bcaba608b099fccbb5e83523e7
Umar-codingal <umarfarukpatelkp@gmail.com>
2026-07-30 10:11:39 +0530

    ww

---
+++ lcm.py
@@ -0,0 +1,8 @@
+import math
+
+smallestNumber = int(input("Enter the smaller number : "))
+biggerNumber = int(input("Enter the bigger number : "))
+
+print(math.lcm(smallestNumber,biggerNumber))
+
+
